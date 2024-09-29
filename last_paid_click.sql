e8e1f79ff7ed19638376d6f350a18df72004e537
Tracksimus <aszhivtsov@gmail.com>
2024-09-29 20:08:54 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,30 +1,38 @@
-with tab as (
-    select distinct on (s.visitor_id)
+WITH tab AS (
+SELECT
+	DISTINCT ON
+	(s.visitor_id)
         s.visitor_id,
-        s.visit_date,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
-        l.lead_id,
-        l.created_at,
-        l.amount,
-        l.closing_reason,
-        l.status_id
-    from sessions as s
-    left join
-        leads as l
-        on s.visitor_id = l.visitor_id and s.visit_date < l.created_at
-    where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    order by
-        s.visitor_id asc,
-        s.visit_date desc
+	s.visit_date,
+	s.source AS utm_source,
+	s.medium AS utm_medium,
+	s.campaign AS utm_campaign,
+	l.lead_id,
+	l.created_at,
+	l.amount,
+	l.closing_reason,
+	l.status_id
+FROM
+	sessions AS s
+LEFT JOIN
+        leads AS l
+        ON
+	s.visitor_id = l.visitor_id
+	AND s.visit_date < l.created_at
+WHERE
+	s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+ORDER BY
+	s.visitor_id ASC,
+	s.visit_date DESC
 )
 
-select *
-from tab
-order by
-    amount desc nulls last,
-    visit_date asc,
-    utm_source asc,
-    utm_medium asc,
-    utm_campaign asc;
+SELECT
+	*
+FROM
+	tab
+ORDER BY
+	amount DESC NULLS LAST,
+	visit_date ASC,
+	utm_source ASC,
+	utm_medium ASC,
+	utm_campaign ASC;
