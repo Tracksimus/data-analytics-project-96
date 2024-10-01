175eebbe3248c847d6a1f3ca29656916561bf6f9
Tracksimus <aszhivtsov@gmail.com>
2024-10-01 23:03:13 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,38 +1,36 @@
 WITH tab AS (
-SELECT
-	DISTINCT ON
-	(s.visitor_id)
+    SELECT DISTINCT ON
+    (s.visitor_id)
         s.visitor_id,
-	s.visit_date,
-	s.source AS utm_source,
-	s.medium AS utm_medium,
-	s.campaign AS utm_campaign,
-	l.lead_id,
-	l.created_at,
-	l.amount,
-	l.closing_reason,
-	l.status_id
-FROM
-	sessions AS s
-LEFT JOIN
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    FROM
+        sessions AS s
+    LEFT JOIN
         leads AS l
         ON
-	s.visitor_id = l.visitor_id
-	AND s.visit_date < l.created_at
-WHERE
-	s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-ORDER BY
-	s.visitor_id ASC,
-	s.visit_date DESC
+            s.visitor_id = l.visitor_id
+            AND s.visit_date < l.created_at
+    WHERE
+        s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    ORDER BY
+        s.visitor_id ASC,
+        s.visit_date DESC
 )
 
-SELECT
-	*
+SELECT *
 FROM
-	tab
+    tab
 ORDER BY
-	amount DESC NULLS LAST,
-	visit_date ASC,
-	utm_source ASC,
-	utm_medium ASC,
-	utm_campaign ASC;
+    amount DESC NULLS LAST,
+    visit_date ASC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC;
