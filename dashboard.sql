7d0ce8ba33528a201cd508d230a85c4f59ab1d3f
Tracksimus <aszhivtsov@gmail.com>
2024-10-05 22:14:27 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -349,3 +349,279 @@ select
     ) - sum(total_cost) as profit
 from
     dataset;
+
+--Расчет кол-ва каналов привлечения
+SELECT count(DISTINCT source)
+FROM sessions;
+
+--Расчет кол-ва общих и уникальных визитов по всем каналам привлечения трафика
+SELECT
+    sessions.source,
+    count(sessions.visitor_id),
+    count(DISTINCT sessions.visitor_id) AS count_distinct
+FROM sessions
+GROUP BY
+    sessions.source;
+
+--Расчет суммарных и уникальных посещений сайта онлайн-школы
+SELECT
+    count(visitor_id),
+    count(DISTINCT visitor_id) AS count_distinct
+FROM sessions;
+
+--Расчет кол-ва лидов
+SELECT sum(leed) AS leads_count
+FROM
+    (SELECT
+        1 AS leed,
+        amount,
+        closing_reason,
+        CASE
+            WHEN amount > 0 THEN 1
+            ELSE 0
+        END AS leed_amount,
+        to_char(date_trunc('day', created_at), 'YYYY-MM-DD') AS date
+    FROM leads
+    ORDER BY date) AS tab21;
+
+--Расчет кол-ва закрытых лидов
+SELECT sum(leed_amount) AS purchases_count
+FROM
+    (SELECT
+        1 AS leed,
+        amount,
+        closing_reason,
+        CASE
+            WHEN amount > 0 THEN 1
+            ELSE 0
+        END AS leed_amount,
+        to_char(date_trunc('day', created_at), 'YYYY-MM-DD') AS date
+    FROM leads
+    ORDER BY date) AS tab22;
+
+--Расчет дохода
+SELECT sum(amount) AS revenue
+FROM
+    (SELECT
+        1 AS leed,
+        amount,
+        closing_reason,
+        CASE
+            WHEN amount > 0 THEN 1
+            ELSE 0
+        END AS leed_amount,
+        to_char(date_trunc('day', created_at), 'YYYY-MM-DD') AS date
+    FROM leads
+    ORDER BY date) AS tab23;--Расчет дохода
+SELECT sum(amount) AS revenue
+FROM
+    (SELECT
+        1 AS leed,
+        amount,
+        closing_reason,
+        CASE
+            WHEN amount > 0 THEN 1
+            ELSE 0
+        END AS leed_amount,
+        to_char(date_trunc('day', created_at), 'YYYY-MM-DD') AS date
+    FROM leads
+    ORDER BY date) AS tab23;--Расчет дохода
+SELECT sum(amount) AS revenue
+FROM
+    (SELECT
+        1 AS leed,
+        amount,
+        closing_reason,
+        CASE
+            WHEN amount > 0 THEN 1
+            ELSE 0
+        END AS leed_amount,
+        to_char(date_trunc('day', created_at), 'YYYY-MM-DD') AS date
+    FROM leads
+    ORDER BY date) AS tab23;
+
+--Расчет расходов
+SELECT sum(total_daily_spent) AS consumption
+FROM
+    (
+        SELECT
+            campaign_name,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            utm_content,
+            campaign_date,
+            sum(daily_spent) AS total_daily_spent
+        FROM
+            (SELECT
+                campaign_name,
+                utm_source,
+                utm_medium,
+                utm_campaign,
+                utm_content,
+                campaign_date,
+                daily_spent
+            FROM vk_ads
+            UNION ALL
+            SELECT
+                campaign_name,
+                utm_source,
+                utm_medium,
+                utm_campaign,
+                utm_content,
+                campaign_date,
+                daily_spent
+            FROM ya_ads) AS combined_ads
+        GROUP BY
+            campaign_name,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            utm_content,
+            campaign_date
+    ) AS tab24;
+
+--Сводная таблица
+with tab as (
+    select
+        sessions.visitor_id,
+        visit_date,
+        source,
+        medium,
+        campaign,
+        created_at,
+        closing_reason,
+        status_id,
+        coalesce(amount, 0) as amount,
+        case
+            when created_at < visit_date then 'delete' else lead_id
+        end as lead_id,
+        row_number()
+            over (partition by sessions.visitor_id order by visit_date desc)
+        as rn
+    from sessions
+    left join leads
+        on sessions.visitor_id = leads.visitor_id
+    where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+tab2 as (
+    select
+        tab.visitor_id,
+        tab.source as utm_source,
+        tab.medium as utm_medium,
+        tab.campaign as utm_campaign,
+        tab.created_at,
+        tab.amount,
+        tab.closing_reason,
+        tab.status_id,
+        date_trunc('day', tab.visit_date) as visit_date,
+        case
+            when tab.created_at < tab.visit_date then 'delete' else lead_id
+        end as lead_id
+    from tab
+    where (tab.lead_id != 'delete' or tab.lead_id is null) and tab.rn = 1
+),
+amount as (
+    select
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        count(visitor_id) as visitors_count,
+        sum(case when lead_id is not null then 1 else 0 end) as leads_count,
+        sum(
+            case
+                when
+                    closing_reason = 'Успешная продажа' or status_id = 142
+                    then 1
+                else 0
+            end
+        ) as purchases_count,
+        sum(amount) as revenue
+    from tab2
+    group by
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign
+),
+tab4 as (
+    select
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
+    from vk_ads
+    union all
+    select
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
+    from ya_ads
+),
+cost as (
+    select
+        campaign_date as visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from tab4
+    group by
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign
+),
+tab5 as (
+select
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        null as revenue,
+        null as visitors_count,
+        null as leads_count,
+        null as purchases_count,
+        total_cost
+    from cost
+    union all
+    select
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        revenue,
+        visitors_count,
+        leads_count,
+        purchases_count,
+        null as total_cost
+    from amount
+),
+tab6 as (
+select
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    sum(coalesce(visitors_count, 0)) as visitors_count,
+    sum(coalesce(total_cost, 0)) as total_cost,
+    sum(coalesce(leads_count, 0)) as leads_count,
+    sum(coalesce(purchases_count, 0)) as purchases_count,
+    sum(coalesce(revenue, 0)) as revenue
+from tab5
+group by
+    utm_source,
+    utm_medium,
+    utm_campaign
+order by total_cost desc
+)
+select *,
+    CASE WHEN visitors_count = 0 THEN NULL ELSE total_cost / visitors_count END AS cpu,
+    CASE WHEN leads_count = 0 THEN NULL ELSE total_cost / leads_count END AS cpl,
+    CASE WHEN purchases_count = 0 THEN NULL ELSE total_cost / purchases_count END AS cppu,
+    CASE WHEN total_cost = 0 THEN NULL ELSE ((revenue - total_cost) / total_cost) * 100 END AS roi
+FROM tab6
+order by roi asc;
